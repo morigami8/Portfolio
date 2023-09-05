ac7a9dc66d178ccec17474ed8243dfecb57e1c1f
Morgan Atwood <atwood.morganj@gmail.com>
2023-09-05 13:39:34 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Header</div>
+      <div>Main</div>
+      <div>Footer</div>
+    </>
+  );
+}
+
+export default App;
