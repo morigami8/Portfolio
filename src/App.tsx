65c1d650bc8f0594378e869517e9c1d0c8e0e87b
Morgan Atwood <atwood.morganj@gmail.com>
2023-09-05 13:43:14 -0700

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import ApplicationCard from './components/Card';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <div>Header</div>
-      <div>Main</div>
-      <div>Footer</div>
+      <Header />
+      <ApplicationCard />
+      <Footer />
     </>
   );
 }
