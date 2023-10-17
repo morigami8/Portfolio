3245549e4467d0d84a3deeb2f3a4d289d52dd059
Morgan Atwood <atwood.morganj@gmail.com>
2023-10-17 15:16:00 -0700

    set up local storage and theme setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
 import Header from './components/Header.tsx';
 import './stylesheets/index.scss';
+import { ThemeContext } from './context/ThemeContext.tsx';
 
 function App() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleOnClick = async () => {
+    await localStorage.setItem('theme', theme);
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
   return (
-    <div className="app dark">
+    <div className={theme}>
       <Header />
+      <button onClick={handleOnClick}>Theme Change</button>
     </div>
   );
 }
