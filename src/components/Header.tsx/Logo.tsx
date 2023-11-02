15105e3335065155b2f284cfa6d38432786378ff
Morgan Atwood <atwood.morganj@gmail.com>
2023-11-02 15:24:08 -0700

    hamburger menu start

--- src/components/Header.tsx/Logo.tsx
+++ src/components/Header.tsx/Logo.tsx
@@ -1,12 +1,9 @@
-import { FaHamburger } from 'react-icons/fa';
+import NavMenu from './NavMenu';
 
 export const Logo = () => {
   return (
     <section className="header-icon flex justify-center items-center">
-      <button className="nav-hidden width-12 height-12 flex rounded-full justify-center items-center hover:bg-primary hover:bg-primary-dark">
-        {' '}
-        <FaHamburger />{' '}
-      </button>
+      <NavMenu />
       <h2 className="flex justify-center items-center">Morgan Atwood</h2>
     </section>
   );
---
+++ src/components/Header.tsx/NavMenu.tsx
@@ -0,0 +1,30 @@
+const NavMenu = () => {
+  return (
+    <div className="nav-hidden hamburger-menu">
+      <input id="menu__toggle" type="checkbox" />
+      <label className="menu__btn" htmlFor="menu__toggle">
+        <span></span>
+      </label>
+
+      <ul className="menu__box">
+        <li>
+          <a className="menu__item" href="#">
+            About Me
+          </a>
+        </li>
+        <li>
+          <a className="menu__item" href="#">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a className="menu__item" href="#">
+            Projects
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavMenu;
