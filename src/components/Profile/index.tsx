052093dcdc601c352fbeb7f9941931db71a1f00d
Morgan Atwood <atwood.morganj@gmail.com>
2023-11-08 15:41:13 -0800

    profile image animation

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,7 +2,17 @@ import styles from './_profile.module.scss';
 const Profile = () => {
   return (
     <div className={`${styles['portfolio-image']} portfolio-image`}>
-      <img src="../../../public/66B3E9E9-B888-43B9-91E0-C6BA740565D7.jpeg" />
+      <div className="box">
+        <div className="content">
+          <img src="../../../public/66B3E9E9-B888-43B9-91E0-C6BA740565D7.jpeg" />
+          <h2>
+            Morgan Atwood
+            <br />
+            <span>Full Stack Developer</span>
+          </h2>
+          <a href="#">Hire</a>
+        </div>
+      </div>
     </div>
   );
 };
