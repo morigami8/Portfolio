077e299a30904306bb2c523f55102fb44d99b12b
Morgan Atwood <atwood.morganj@gmail.com>
2024-01-17 15:37:26 -0800

    carousel init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ViteSass from 'vite-plugin-sass';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ViteSass()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
