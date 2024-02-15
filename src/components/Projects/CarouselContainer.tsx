a4a6f74903b2c57e0220543d0fc22106ddce0b4d
Morgan Atwood <atwood.morganj@gmail.com>
2024-02-15 15:22:05 -0800

    project carousel arrows

--- src/components/Projects/CarouselContainer.tsx
+++ src/components/Projects/CarouselContainer.tsx
@@ -10,10 +10,14 @@ const CarouselContainer = ({ title, list }: CarouselProject) => {
   return (
     <div className="px-12 flex flex-col items-center">
       <h2>{title}</h2>
-      <Carousel className="w-full max-w-sm">
+      <Carousel className="carousel max-w-sm">
         <CarouselProjectItem list={list} title={''} />
-        <CarouselPrevious />
-        <CarouselNext />
+        {list.length > 1 && (
+          <>
+            <CarouselPrevious />
+            <CarouselNext />
+          </>
+        )}
       </Carousel>
     </div>
   );
