39b61d92fe555fe477d1b773d2405248e42f9cf7
Morgan Atwood <atwood.morganj@gmail.com>
2024-03-07 14:23:53 -0800

    click outside nav menu

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -1,12 +1,41 @@
+import React, { useState, useEffect, useRef } from 'react';
+
 const NavMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false); // Close the menu
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
+
   return (
-    <div className="nav-hidden hamburger-menu">
-      <input id="menu__toggle" type="checkbox" />
+    <div className="nav-hidden hamburger-menu" ref={menuRef}>
+      <input
+        id="menu__toggle"
+        type="checkbox"
+        checked={isMenuOpen}
+        onChange={toggleMenu}
+      />
       <label className="menu__btn" htmlFor="menu__toggle">
         <span></span>
       </label>
 
-      <ul className="menu__box">
+      <ul className={`menu__box ${isMenuOpen ? '' : 'hidden'}`}>
+        {' '}
+        {/* Add a class to hide/show the menu */}
         <li>
           <a className="menu__item" href="#about-me">
             About Me
