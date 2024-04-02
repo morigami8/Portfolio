5ae0f9b5a5af55081117bc35d66dac065bd46be1
Morgan Atwood <atwood.morganj@gmail.com>
2024-04-02 16:13:11 -0700

    resume component styles

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,10 +1,18 @@
 import MyResume from '../../../public/resume.pdf';
+import styles from './_resume.module.scss';
 
 const Resume = () => {
   return (
-    <div>
+    <div
+      className={`${styles['resume-container']} resume-container py-4`}
+      id="resume"
+    >
       <h2>
-        Please <a href={MyResume}> download</a> my resume.
+        <a target="_blank" href={MyResume}>
+          {' '}
+          <span>Download/View</span>
+        </a>{' '}
+        my resume
       </h2>
     </div>
   );
